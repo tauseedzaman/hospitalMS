4694de07373f169b04e3052e90bfb8ca0e33bad3
tauseedzaman <tauseedzamanofsorana@gmail.com>
2022-08-11 07:50:02 -0700

    routes bug fixed

--- routes/web.php
+++ routes/web.php
@@ -36,11 +36,11 @@ Route::middleware(['auth','checksuperadmin'])->group(function () {
 
         Route::get('/docters',App\Http\Livewire\Admins\Docter::class)->name('admin_docters');
 
-        Route::get('/operationsreport',App\Http\Livewire\Admins\operationreport::class)->name('admin_operations_report');
+        Route::get('/operationsreport',App\Http\Livewire\Admins\Operationreport::class)->name('admin_operations_report');
 
         Route::get('/patients',App\Http\Livewire\Admins\Patients::class)->name('admin_patients');
 
-        Route::get('/birthsreport',App\Http\Livewire\Admins\birthreport::class)->name('admin_birth_report');
+        Route::get('/birthsreport',App\Http\Livewire\Admins\Birthreport::class)->name('admin_birth_report');
 
          Route::get('/patientBills',App\Http\Livewire\Admins\Bills::class)->name('patient_bills');
 
@@ -50,17 +50,17 @@ Route::middleware(['auth','checksuperadmin'])->group(function () {
 
         Route::get('/medicinesStore',App\Http\Livewire\Admins\Medicinestore::class)->name('medicinesStore');
 
-        Route::get('/departments',App\Http\Livewire\Admins\departments::class)->name('departments');
+        Route::get('/departments',App\Http\Livewire\Admins\Departments::class)->name('departments');
 
-        Route::get('/employees',App\Http\Livewire\Admins\employees::class)->name('employees');
+        Route::get('/employees',App\Http\Livewire\Admins\Employees::class)->name('employees');
 
         Route::get('/appointment',App\Http\Livewire\Admins\Appiontment::class)->name('appointment');
 
-        Route::get('/blocks',App\Http\Livewire\Admins\blocks::class)->name('blocks');
+        Route::get('/blocks',App\Http\Livewire\Admins\Blocks::class)->name('blocks');
 
-        Route::get('/admin/hods',App\Http\Livewire\Admins\hods::class)->name('hods');
+        Route::get('/admin/hods',App\Http\Livewire\Admins\Hods::class)->name('hods');
 
-        Route::get('/admin/requestedappointments',App\Http\Livewire\Admins\requestedAppointments::class)->name('requestedAppointment');
+        Route::get('/admin/requestedappointments',App\Http\Livewire\Admins\RequestedAppointments::class)->name('requestedAppointment');
 
         Route::get('/subscribers',App\Http\Livewire\Admins\Subscibers ::class)->name('subscibers');
 
