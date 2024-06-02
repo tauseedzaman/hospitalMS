5c8ca85ccba21ec90a11d985939cb86de8159755
tauseedzaman <tauseedzaman@protonmail.com>
2024-06-02 15:08:05 +0500

    routes files changed

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+// testing gpush
 Route::get('/', function () {
     return view('index');
 });
